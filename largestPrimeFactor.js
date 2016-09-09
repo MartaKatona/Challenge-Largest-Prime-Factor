4efd99275e1a6ae04ca7d859b908d7bdff7894d9
Crescendi <martahonolulu@hotmail.com>
2016-09-08 17:28:24 -1000

    done

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -2,5 +2,14 @@ exports.largestPrimeFactor = function(n){
   var primeNumber = 0;
   // do your work here
 
+  primeNumber  = 2;
+  while (primeNumber <= n){
+    if (n%primeNumber === 0){
+        n /= primeNumber;
+    } else {
+      primeNumber++;
+    }
+  }
+
   return primeNumber;
 };
